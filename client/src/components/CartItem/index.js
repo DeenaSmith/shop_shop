44c53f1f3183bae72d787da5b4a327bfb7794ce9
Deena Smith <deenasmith1630@gmail.com>
2022-07-03 11:03:05 -0500

    saved items remain on refresh

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,4 +1,5 @@
 
+import { idbPromise } from "../../utils/helpers";
 import React from 'react';
 import { useStoreContext } from "../../utils/GlobalState";
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
@@ -13,6 +14,7 @@ const CartItem = ({ item }) => {
             type: REMOVE_FROM_CART,
             _id: item._id
         });
+        idbPromise('cart', 'delete', { ...item });
     };
 
     const onChange = (e) => {
@@ -22,12 +24,16 @@ const CartItem = ({ item }) => {
                 type: REMOVE_FROM_CART,
                 _id: item._id
             });
+
+            idbPromise('cart', 'delete', { ...item });
         } else {
             dispatch({
                 type: UPDATE_CART_QUANTITY,
                 _id: item._id,
                 purchaseQuantity: parseInt(value)
             });
+
+            idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
         }
     }
 
